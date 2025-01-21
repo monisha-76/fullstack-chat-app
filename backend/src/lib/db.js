edd4525adbcb378724d73001d9b02ded0301ad36
monisha-76 <monishamoorthy.16@gmail.com>
2025-01-21 22:53:27 +0530

    project completed

---
+++ backend/src/lib/db.js
@@ -0,0 +1,10 @@
+import mangoose from "mongoose";
+
+export const connectDB = async() =>{
+    try{
+        const conn = await mangoose.connect(process.env.MONGODB_URI);
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error){
+        console.log("MongoDB connection error:", error);
+    }
+};
